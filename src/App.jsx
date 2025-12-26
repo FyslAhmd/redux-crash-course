3004e6382fe089b5ffaa448afb079f05c5446316
FyslAhmd <info.faysal.32@gmail.com>
2025-12-27 00:03:42 +0600

    lift u count value in manual form

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,49 @@
+import { useState } from "react";
 import "./App.css";
 import Counter from "./components/Counter";
 import Stats from "./components/Stats";
 
+const initialCounters = [
+  {
+    id: 1,
+    value: 0,
+  },
+  {
+    id: 2,
+    value: 0,
+  },
+];
 function App() {
+  const [counters, setCounters] = useState(initialCounters);
+
+  const totalCount = counters.reduce((sum, current) => sum + current.value, 0);
+
+  const handleIncrement = (id) => {
+    const updatedCounters = counters.map((counter) => {
+      if (counter.id === id) {
+        return {
+          ...counter,
+          value: counter.value + 1,
+        };
+      }
+      return counter;
+    });
+    setCounters(updatedCounters);
+  };
+
+  const handleDecrement = (id) => {
+    const updatedCounters = counters.map((counter) => {
+      if (counter.id === id) {
+        return {
+          ...counter,
+          value: counter.value - 1,
+        };
+      }
+      return counter;
+    });
+    setCounters(updatedCounters);
+  };
+
   return (
     <>
       <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
@@ -11,9 +52,15 @@ function App() {
         </h1>
 
         <div className="max-w-md mx-auto mt-10 space-y-5">
-          <Counter />
-          <Counter />
-          <Stats totalCount={10} />
+          {counters.map((counter) => (
+            <Counter
+              key={counter.id}
+              count={counter.value}
+              onIncrement={() => handleIncrement(counter.id)}
+              onDecrement={() => handleDecrement(counter.id)}
+            />
+          ))}
+          <Stats totalCount={totalCount} />
         </div>
       </div>
     </>
