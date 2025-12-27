5178786a75a0287b8725612cf19b08937f5caefe
FyslAhmd <info.faysal.32@gmail.com>
2025-12-27 13:01:23 +0600

    apply redux to counter function

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,21 @@
-import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import Counter from "./components/Counter";
 import Stats from "./components/Stats";
+import { decrement, increment } from "./features/counters/counterSlice";
 
-const initialCounters = [
-  {
-    id: 1,
-    value: 0,
-  },
-  {
-    id: 2,
-    value: 0,
-  },
-];
 function App() {
-  const [counters, setCounters] = useState(initialCounters);
+  const counters = useSelector((state) => state.counters);
+  const dispatch = useDispatch();
 
   const totalCount = counters.reduce((sum, current) => sum + current.value, 0);
 
   const handleIncrement = (id) => {
-    const updatedCounters = counters.map((counter) => {
-      if (counter.id === id) {
-        return {
-          ...counter,
-          value: counter.value + 1,
-        };
-      }
-      return counter;
-    });
-    setCounters(updatedCounters);
+    dispatch(increment(id));
   };
 
   const handleDecrement = (id) => {
-    const updatedCounters = counters.map((counter) => {
-      if (counter.id === id) {
-        return {
-          ...counter,
-          value: counter.value - 1,
-        };
-      }
-      return counter;
-    });
-    setCounters(updatedCounters);
+    dispatch(decrement(id));
   };
 
   return (
